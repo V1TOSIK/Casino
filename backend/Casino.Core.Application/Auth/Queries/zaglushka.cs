74d51824228f955f7c1a7b25045e20fbbf5a2c7a
Vitos <vitalioostrovski@gmail.com>
2025-11-17 13:40:46 +0200

    add some code, db configuration, and auth command

---
+++ backend/Casino.Core.Application/Auth/Queries/zaglushka.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Core.Application.Auth.Queries
+{
+    internal class zaglushka
+    {
+    }
+}
---
+++ backend/Casino.Core.Application/DependencyInjection/ApplicationInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Casino.Core.Application.DependencyInjection
+{
+    public static class ApplicationInjection
+    {
+        public static IServiceCollection AddApplicationInjection(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
